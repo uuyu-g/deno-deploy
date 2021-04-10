b02e3436ca899ecbf12f4eead228d97becf91d79
yutaro <aqueous.mkli@gmail.com>
2021-04-10 14:55:17 +0900

    add auth

--- datadeno.ts
+++ datadeno.ts
@@ -1,20 +1,22 @@
 import { serve } from "https://deno.land/x/sift@0.1.7/mod.ts";
 
-const SLACK_URL = Deno.env.get("SLACK_URL");
-
-if (!SLACK_URL) {
-  throw new Error("SLACK_URLが設定されていません");
+const SLACK_TOKEN = Deno.env.get("SLACK_TOKEN");
+if (!SLACK_TOKEN) {
+  throw new Error("環境変数が設定されていません");
 }
+const SLACK_URL = `https://hooks.slack.com/services/${SLACK_TOKEN}`;
 
 serve({
   "/:name": (req, params) => {
-    fetch(
+    const token = req.headers.get("Authentication");
+    if (token !== SLACK_TOKEN) return new Response("権限がありません");
+
+    return fetch(
       SLACK_URL,
       {
         method: "POST",
         body: JSON.stringify({ text: `Hello, ${params?.name}!` }),
       },
     );
-    return new Response("send!");
   },
 });
